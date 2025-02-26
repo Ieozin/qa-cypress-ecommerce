57645a15310814fdbe259967045c5d4d0c5f790b
Leonardo Martins <leonardomartinslm9090@gmail.com>
2025-02-26 20:57:11 -0300

    Implementado teste de Intercept

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,12 +1,38 @@
 describe("Fluxo de Checkout", () => {
-  before(() => {
+  beforeEach(() => {
     cy.setCookie("ebacStoreVersion", "v2", {
       domain: "lojaebac.ebaconline.art.br",
     });
   });
 
-  it("Deve adicionar um produto ao carrinho e concluir a compra ", () => {
+  it("Deve interceptar e validar a adição de um item no carrinho", () => {
+    cy.intercept("GET", "**/public/getCart?userId=*", (req) => {
+      req.reply({
+        statusCode: 200,
+        body: {
+          success: true,
+          message: "Item adicionado com sucesso",
+        },
+      });
+    }).as("getCarrinho");
+
     cy.visit("/");
-    cy.adicionarProduto('Teste')
+    cy.adicionarProduto("Teste");
+    cy.wait("@getCarrinho").its("response.statusCode").should("eq", 200);
+  });
+
+  it("Deve intercerpectar e valitar uma remoção de um item no carrinho", () => {
+    cy.intercept("PUT", "**/public/updateCart/*", (req) => {
+      req.reply({
+        statusCode: 200,
+        body: {
+          sucess: true,
+          message: "Item removido com sucesso",
+        },
+      });
+    }).as("putCarrinho");
+
+    cy.visit("/");
+    cy.adicionarProduto("Teste");
   });
 });
