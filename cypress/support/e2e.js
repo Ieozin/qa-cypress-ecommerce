099975afcd3985fbf3595e24df5d71b7c50583fb
Leonardo Martins <leonardomartinslm9090@gmail.com>
2025-05-12 20:41:41 -0300

    testando fix da CI key 3

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,8 +1,9 @@
+import "./commands";
+
 Cypress.on("uncaught:exception", (err, runnable) => {
   if (err.message.includes("First argument must be a string")) {
     console.warn("⚠️ Erro da aplicação ignorado pelo Cypress:", err.message);
     return false;
   }
-
   return true;
 });
